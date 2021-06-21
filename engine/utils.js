0d828ede720430053da6a7c3499f5e79656364d4
siisgoo <siisgoo@mail.ru>
2021-06-21 23:34:58 +0300

    modified:   colorscheme.scss 	modified:   engine/utils.js 	modified:   index.html 	modified:   pages/contacts.html 	modified:   pages/documents.html 	modified:   pages/news.html 	modified:   pages/prices.html 	modified:   pages/production.html 	modified:   pages/production/misc/plastic-products.html 	modified:   pages/production/misc/playgrounds-equipment.html 	modified:   pages/production/misc/universal-construction-mechanism.html 	modified:   pages/production/style.css 	modified:   pages/production/sub-prod-template.html 	modified:   pages/production/water-heaters/autonomus-gas-heating-boilers.html 	modified:   pages/production/water-heaters/e-type-heaters.html 	modified:   pages/production/water-heaters/electric-steam-generators-electrode-type-200.html 	modified:   pages/production/water-heaters/electric-steam-generators-electrode-type-25.html 	modified:   pages/production/water-heaters/electric-steam-generators-electrode-type-60.html 	modified:   pages/production/water-heaters/electric-steam-generators-electrode-type-90.html 	modified:   pages/production/water-heaters/electric-steam-generators-electrode-type.html 	modified:   pages/production/water-heaters/independent-mobile-block-modular-boiler.html 	modified:   pages/production/water-heaters/independent-mobile-modular-electric-boilers.html 	new file:   pages/reviews.html 	modified:   prodStyle.scss 	modified:   pub/style.css 	modified:   style.scss 	deleted:    tmp

--- engine/utils.js
+++ engine/utils.js
@@ -16,3 +16,32 @@ function toggleSub(sm) {
 		sm.text.className = "fa fa-angle-down";
 	}
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+	let goupBtn = document.getElementById("go_up");
+
+	window.onscroll = function() {
+		let offset = window.pageYOffset;
+		if (offset > 103) {
+			goupBtn.style.opacity = "0.5";
+			goupBtn.style.display = "block";
+			goupBtn.style.top = "0";
+		// } else if (offset < 70 && offset > 40) {
+		// 	goupBtn.style.opacity = "0.3";
+		// 	goupBtn.style.display = "block";
+		// } else if (offset < 40 && offset > 10) {
+		// 	goupBtn.style.opacity = "0.1";
+		// 	goupBtn.style.display = "block";
+		} else {
+			goupBtn.style.opacity = "0";
+			goupBtn.style.display = "none";
+		}
+	};
+
+	goupBtn.addEventListener('click', function () {
+		window.scrollBy({
+			top: -document.documentElement.scrollHeight,
+			behavior: 'smooth'
+		});
+	});
+});
