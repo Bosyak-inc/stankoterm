a249f7d252def55bd262c12ae42e9d443042ec7f
siisgoo <siisgoo@mail.ru>
2021-06-22 23:09:57 +0300

    modified:   engine/utils.js 	modified:   index.html 	modified:   pages/contacts.html 	modified:   pages/documents.html 	modified:   pages/news.html 	modified:   pages/prices.html 	modified:   pages/production.html 	modified:   pages/production/misc/plastic-products.html 	modified:   pages/production/misc/playgrounds-equipment.html 	modified:   pages/production/misc/universal-construction-mechanism.html 	modified:   pages/production/poultry-equipment/axial-fan-1.html 	modified:   pages/production/poultry-equipment/axial-fan-2.html 	modified:   pages/production/poultry-equipment/axial-fan-3.html 	modified:   pages/production/poultry-equipment/birds-transport-containers.html 	modified:   pages/production/poultry-equipment/incubation-trolley.html 	modified:   pages/production/poultry-equipment/motorized-air-valve.html 	modified:   pages/production/poultry-equipment/universal-built-mechanism.html 	modified:   pages/production/style.css 	modified:   pages/production/sub-prod-template.html 	modified:   pages/production/water-heaters/autonomus-gas-heating-boilers.html 	modified:   pages/production/water-heaters/e-type-heaters.html 	modified:   pages/production/water-heaters/electric-steam-generators-electrode-type-200.html 	modified:   pages/production/water-heaters/electric-steam-generators-electrode-type-25.html 	modified:   pages/production/water-heaters/electric-steam-generators-electrode-type-60.html 	modified:   pages/production/water-heaters/electric-steam-generators-electrode-type-90.html 	modified:   pages/production/water-heaters/electric-steam-generators-electrode-type.html 	modified:   pages/production/water-heaters/independent-mobile-block-modular-boiler.html 	modified:   pages/production/water-heaters/independent-mobile-modular-electric-boilers.html 	modified:   pages/reviews.html 	modified:   prodStyle.scss 	modified:   pub/style.css 	modified:   style.scss 	modified:   template.html

--- engine/utils.js
+++ engine/utils.js
@@ -1,41 +1,78 @@
-function SubMenu(bt, mn, txt, st = 0) {
-	this.stat   = st;
-	this.button = bt;
-	this.menu   = mn;
-	this.text   = txt;
+function elemHeight(elem) {
+	let style = getComputedStyle(elem);
+	return elem.offsetHeight +
+					parseInt(style.marginTop) +
+					parseInt(style.marginBottom);
 }
 
-function toggleSub(sm) {
-	if (sm.stat === 0) {
-		sm.stat = 1;
-		sm.menu.style.display = "block";
-		sm.text.className = "fa fa-angle-up";
+function elemWidth(elem) {
+	let style = getComputedStyle(elem);
+	return elem.offsetWidth +
+					parseInt(style.marginRight) +
+					parseInt(style.marginLeft);
+}
+
+function goupCheck(goup, treshold) {
+	function goupScale(goup, state, opacity) {
+		if (state === 1) {
+			goup.style.opacity = opacity;
+			goup.style.display = "block";
+			goup.style.top = 0;
+		} else {
+			goup.style.display = "none";
+		}
+	}
+
+	let offset = window.pageYOffset;
+	if (offset >= treshold) {
+		goupScale(goup, 1, 0.5);
 	} else {
-		sm.stat = 0;
-		sm.menu.style.display = "none";
-		sm.text.className = "fa fa-angle-down";
+		goupScale(goup, 0, 0.5);
 	}
 }
 
+function navMove(nav, navY) {
+	let offset = window.pageYOffset;
+	let diff = navY - offset;
+	if (diff < 0) {
+		nav.style.top = "0" + 'px';
+	} else {
+		nav.style.top = diff + 'px';
+	}
+}
+
+function SubElem(button, subBlock, buttonSkin, state = 0) {
+	this.state      = state;
+	this.button     = button;
+	this.subBlock   = subBlock;
+	this.buttonSkin = buttonSkin;
+
+	this.toggleSub = function() {
+		if (this.state === 0) {
+			this.state = 1;
+			this.subBlock.style.display = "block";
+			this.buttonSkin.className = "fa fa-angle-up";
+		} else {
+			this.state = 0;
+			this.subBlock.style.display = "none";
+			this.buttonSkin.className = "fa fa-angle-down";
+		}
+	};
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+	let header = document.getElementById("header");
+	let nav = document.getElementById("nav");
+
+	let headerHeight = elemHeight(header);
+	let navY = nav.offsetTop;
+
+	// go up button util
 	let goupBtn = document.getElementById("go_up");
 
 	window.onscroll = function() {
-		let offset = window.pageYOffset;
-		if (offset > 103) {
-			goupBtn.style.opacity = "0.5";
-			goupBtn.style.display = "block";
-			goupBtn.style.top = "0";
-		// } else if (offset < 70 && offset > 40) {
-		// 	goupBtn.style.opacity = "0.3";
-		// 	goupBtn.style.display = "block";
-		// } else if (offset < 40 && offset > 10) {
-		// 	goupBtn.style.opacity = "0.1";
-		// 	goupBtn.style.display = "block";
-		} else {
-			goupBtn.style.opacity = "0";
-			goupBtn.style.display = "none";
-		}
+		goupCheck(goupBtn, headerHeight);
+		navMove(nav, navY);
 	};
 
 	goupBtn.addEventListener('click', function () {
@@ -43,5 +80,22 @@ document.addEventListener('DOMContentLoaded', () => {
 			top: -document.documentElement.scrollHeight,
 			behavior: 'smooth'
 		});
+	});
+
+	// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+	// sub menu util
+	let buttonedC = document.getElementsByClassName("buttoned");
+	let buttoned = new Array(); 
+
+	let i = 0;
+	Array.from(buttonedC).forEach(function(el) {
+		let button = el.getElementsByClassName("button-container")[0];;
+		let subBlock = el.getElementsByClassName("subBlock")[0];
+		let buttonSkin = button.getElementsByClassName("skin")[0];
+
+		buttoned[i] = new SubElem(button, subBlock, buttonSkin, 0);
+		let tmp = buttoned[i];
+		buttoned[i].button.addEventListener("click", function(){ tmp.toggleSub(); });
+		i++;
 	});
 });
